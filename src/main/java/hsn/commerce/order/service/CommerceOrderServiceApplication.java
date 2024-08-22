ad1a8de1018880c69f8a2c4f67e32b4baf448dea
hasanej <eljabirhasan@gmail.com>
2024-08-22 19:50:38 +0700

    Add project structure

---
+++ src/main/java/hsn/commerce/order/service/CommerceOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package hsn.commerce.order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hsn/commerce/order/service/CommerceOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hsn.commerce.order.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
